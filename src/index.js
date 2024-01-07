3f45ce51009a5580de266e9100f113e620271af8
Roshan Singh <roshan2152@gmail.com>
2024-01-08 00:48:55 +0530

    Custom API response and error handling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ dotenv.config({
 })
 
 connectDB()
-
+.then( () => {
+    app.listen(process.env.PORT || 8000);
+    console.log("Server is running at port: ${process.env.PORT}")
+})
+.catch( (error) => {
+    console.log("MongoDB connection failed", error)
+})
 
 
 // .then(() => {

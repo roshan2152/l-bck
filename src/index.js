d77f8edf09f7dfc48bbab2fee7b4dd2283340a98
Roshan Singh <roshan2152@gmail.com>
2024-01-09 00:55:56 +0530

    Added multer and cloudinary with controller implementation for user registeration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import {app} from './app.js'
+import {app} from './app.js'
 dotenv.config({  
     path: './.env'
 })
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
 .then( () => {
     app.listen(process.env.PORT || 8000);
-    console.log("Server is running at port: ${process.env.PORT}")
+    console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
 })
 .catch( (error) => {
     console.log("MongoDB connection failed", error)
